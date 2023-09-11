ca019dbfcc4052da4ea4c5b9331657ca6fa10735
riotamoriya <riotamoriya@gmail.com>
2023-09-11 18:25:24 +0900

    fixed the Bar styles: make letter bigger

--- src/components/ColorVariation/Bar/index.jsx
+++ src/components/ColorVariation/Bar/index.jsx
@@ -6,9 +6,13 @@ import './style.scss';
 const Bar = ({selectedAngleID, selectedBodyDim3, selectedHandleDim3}) => {
   
   return (
-      <div>
-        <span className="title">{`【${angles[selectedAngleID].MEMO}】品番: ${bodyColors[selectedBodyDim3[2]].NAME}#${handleColors[selectedHandleDim3[2]].NAME}`}</span>
-        <span ></span>
+      <div className="title">
+        <span className="angle">
+          {`【${angles[selectedAngleID].MEMO}】品番: `}
+        </span>
+        <span className="product-id">
+          {`${bodyColors[selectedBodyDim3[2]].NAME}#${handleColors[selectedHandleDim3[2]].NAME}`}
+        </span>
       </div>
 
   )
