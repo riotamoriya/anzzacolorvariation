ec005c81aa9d033046436683789aaf85cf8e36af
riotamoriya <riotamoriya@gmail.com>
2023-10-23 13:32:58 +0900

    Optimized app.jsx, app.scss

--- src/components/ColorVariation/cores/constants.js
+++ src/components/ColorVariation/cores/constants.js
@@ -1,6 +1,14 @@
-// parts
+//
+// The constant Value in this project. 
+//
+
+// Parts 
 export const ID_OF_BODY = 0;
 export const ID_OF_HANDLE = 1;
+
+// Angles
+export const ID_OF_FRONTAL = 0;
+export const ID_OF_OBLIQUE = 1;
 
 
 
--- src/components/ColorVariation/index.jsx
+++ src/components/ColorVariation/index.jsx
@@ -1,62 +1,65 @@
 import React, { useState } from "react";
 
+import {
+    ID_OF_BODY, ID_OF_HANDLE, ID_OF_FRONTAL,
+} from "./cores/constants";
 
-import { ID_OF_BODY, ID_OF_HANDLE} from "./cores/constants";
 
 import ColorPicker from "./ColorPicker";
 import LayersFlame from "./LayersFlame";
 import Bar from "./Bar";
 import Angle from "./Angle";
 import PartSelections from "./PartsSelections";
-// import Notice from "./Notice";
 
 import './style.scss';
 
 const ColorVariation = () => {
-    const [selectedAngleID, setSelectedAngleID] = useState(0); // the front
-    const [selectedPartID, setSelectedPartID] = useState(0); // 
-    const [selectedColorID, setSelectedColorID] = useState(0); // 
 
+    // The states of current selected pattern IDs by Users.
+    const [selectedAngleID, setSelectedAngleID] = useState(ID_OF_FRONTAL); // The Angle (is initialized as FRONTAL)
+    const [selectedPartID, setSelectedPartID] = useState(ID_OF_BODY); // The Part of bag (is initialized as BODY)
+    const [selectedColorID, setSelectedColorID] = useState(null); // The Color of bag (is initialized as null because no problem any colors at first point)
 
-    // [angle, part, color]
-    const [selectedBodyDim3, setSelectedBodyDim3] = useState([0,ID_OF_BODY,6]);
-    const [selectedHandleDim3, setSelectedHandleDim3] = useState([0,ID_OF_HANDLE,2]);
+    // The displayed pattern of layers which are components of the Whole Image.
+    const [selectedBodyDim3, setSelectedBodyDim3] = useState([ID_OF_FRONTAL, ID_OF_BODY, 6]);
+    const [selectedHandleDim3, setSelectedHandleDim3] = useState([ID_OF_FRONTAL, ID_OF_HANDLE, 2]);
 
     return (
         <div className="color-variation-container">
+            {/* The top of UI: The information for users. */}
             <div className="bar-section">
-                <Bar 
-                    selectedAngleID={selectedAngleID} 
+                <Bar
+                    selectedAngleID={selectedAngleID}
                     selectedBodyDim3={selectedBodyDim3} selectedHandleDim3={selectedHandleDim3}
                 />
             </div>
-
-            {/* <div className="notice-section">
-                <Notice />
-            </div> */}
-
+            
+            {/* The left of UI: The controller of Angles selection */}
             <div className="angle-section">
                 <Angle setSelectedAngleID={setSelectedAngleID}
-                    selectedBodyDim3={selectedBodyDim3} setSelectedBodyDim3={setSelectedBodyDim3} 
+                    selectedBodyDim3={selectedBodyDim3} setSelectedBodyDim3={setSelectedBodyDim3}
                     selectedHandleDim3={selectedHandleDim3} setSelectedHandleDim3={setSelectedHandleDim3}
                 />
             </div>
 
+            {/* The center of UI: The display of images */}
             <div className="layerFlame-section">
-                <LayersFlame 
+                <LayersFlame
                     selectedAngleID={selectedAngleID}
-                    selectedPartID={selectedPartID} 
+                    selectedPartID={selectedPartID}
                     selectedColorID={selectedColorID}
                     selectedBodyDim3={selectedBodyDim3} selectedHandleDim3={selectedHandleDim3}
                 />
             </div>
 
+            {/* The ceiling of UI: The controller of Parts selection */}
             <div className="parts-section">
                 <PartSelections setSelectedPartsID={setSelectedPartID} />
             </div>
 
+            {/* The right of UI: The controller of Colors selection */}
             <div className="color-picker-section">
-                <ColorPicker 
+                <ColorPicker
                     selectedAngleID={selectedAngleID} selectedPartID={selectedPartID} setSelectedColorID={setSelectedColorID}
                     setSelectedBodyDim3={setSelectedBodyDim3} setSelectedHandleDim3={setSelectedHandleDim3}
                 />
